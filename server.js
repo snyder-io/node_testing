c2d6e481ddad8cdddd0848d42ce2726f933ba859
Tushar K A <tushar.whitecollar@gmail.com>
2024-06-18 16:05:08 +0530

    test

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const http = require('http');
+const port = 8080;
 const fs = require('fs');
-const index = fs.readFileSync('index.html','utf-8');
-const data = fs.readFileSync('data.json','utf-8');
 
+const index = fs.readFileSync('index.html', 'utf-8');
+const data = fs.readFileSync('data.json', 'utf-8');
 
-const server = http.createServer((req, res)=>{
-
+const server = http.createServer((req,res)=>{
     console.log(req.url);
-    console.log('Server is live...');
-    res.setHeader('Demo', 'DemoValue');
-    // res.setHeader('Content-Type','text/html')
-    res.setHeader('Content-Type', 'application/json');
-
-    res.end(data);
-
+    console.log(`Server is live on port ${port}`);
+    res.setHeader('Content-Type', 'text/html');
+    res.end(index);
 });
 
-server.listen(8080);
+server.listen(port);
